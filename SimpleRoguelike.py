0bd8535f6d385adf8f1b10e518fe845bc90ea98c
Doug Rorschach <34867563+Dougvacs@users.noreply.github.com>
2021-06-18 19:13:09 +0200

    Hello World Window

--- SimpleRoguelike.py
+++ SimpleRoguelike.py
@@ -1,3 +1,26 @@
 import esper as ecs
-import bearlibterminal as blt
+from bearlibterminal import terminal as blt
 
+
+
+def main():
+    screen_width = 160
+    screen_height = 50
+    
+    blt.open()
+    blt.setf(
+        "window: size={}x{}, resizeable=true;", 
+        screen_width, screen_height)
+    blt.setf("font: ../DroidSansMono.ttf, size=12")
+    blt.clear()
+    blt.refresh()
+    while True:
+        blt.clear()
+        blt.printf(2,1,"Hello World!")
+        blt.refresh()
+        if blt.read() == blt.TK_CLOSE:
+            break
+    blt.close()
+
+if __name__ == "__main__":
+    main()
